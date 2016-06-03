96a4a69693330d5ea2fee0a83a88777c4d888827
Curtis Smithson <curtis.smithson@settuk.com>
2016-06-03 11:02:28 +0100

    French ResX Added

---
+++ Create New Network.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WiFi_Connector
+{
+    partial class Create_New_Network
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblSSID = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(36, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // lblSSID
+            // 
+            this.lblSSID.AutoSize = true;
+            this.lblSSID.Location = new System.Drawing.Point(33, 30);
+            this.lblSSID.Name = "lblSSID";
+            this.lblSSID.Size = new System.Drawing.Size(78, 13);
+            this.lblSSID.TabIndex = 2;
+            this.lblSSID.Text = "Network Name";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(33, 84);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Create_New_Network
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 168);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblSSID);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Create_New_Network";
+            this.Text = "Create_New_Network";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblSSID;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Create New Network.cs
@@ -0,0 +1,127 @@
+﻿using NativeWifi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WiFi_Connector
+{
+    public partial class Create_New_Network : Form
+    {
+        private Process newProcess = new Process();
+
+        public Create_New_Network()
+        {
+            InitializeComponent();
+            newProcess.StartInfo.UseShellExecute = false;
+            newProcess.StartInfo.CreateNoWindow = true;
+            newProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            InitializeComponent();
+        }
+
+        public bool isUserAdmin()
+        {
+            bool isAdmin;
+
+            try
+            {
+                WindowsIdentity userIdentity = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(userIdentity);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                isAdmin = false;
+            }
+            catch (Exception)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
+        }
+
+        public void StopBroadcasting()
+        {
+            newProcess.StartInfo.FileName = "netsh";
+            newProcess.StartInfo.Arguments = "wlan stop hostednetwork";
+
+            try
+            {
+                using (Process execute = Process.Start(newProcess.StartInfo))
+                    execute.WaitForExit();
+                SetWlanDetails();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void StartBroadcasting()
+        {
+            newProcess.StartInfo.FileName = "netsh";
+            newProcess.StartInfo.Arguments = "wlan start hostednetwork";
+
+            try
+            {
+                using (Process execute = Process.Start(newProcess.StartInfo))
+                    execute.WaitForExit();
+                button1.Text = "Stop";
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void SetWlanDetails()
+        {
+            newProcess.StartInfo.FileName = "netsh";
+            newProcess.StartInfo.Arguments = "wlan set hostednetwork mode=allow" + textBox1.Text + " key=" + textBox2.Text;
+
+            try
+            {
+                using (Process execute = Process.Start(newProcess.StartInfo))
+                    execute.WaitForExit();
+                StartBroadcasting();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void StopProcess()
+        {
+            newProcess.StartInfo.FileName = "netsh";
+            newProcess.StartInfo.Arguments = "wlan stop hostednetwork";
+
+            try
+            {
+                using (Process execute = Process.Start(newProcess.StartInfo))
+                    execute.WaitForExit();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Start")
+            {
+                StopBroadcasting();
+                button1.Text = "Stop";
+            }
+            else
+            {
+                StopProcess();
+                button1.Text = "Start";
+            }
+        }
+    }
+}
